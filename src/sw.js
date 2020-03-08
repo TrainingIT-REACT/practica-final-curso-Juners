34004e9dbbf395b168690ec18c2519f90737b24b
Juners <13680059+Juners@users.noreply.github.com>
2020-03-08 15:58:06 +0100

    Fixed the service worker

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,12 @@
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
 self.addEventListener('install', (event) => {
   console.log("El service worker ha sido instalado!");
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('activado');
 });
 
 self.addEventListener('message', (e) => {
